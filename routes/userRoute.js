0b08774c08920e8ce0205f980b80ad2720ca9fd2
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-08-17 17:31:01 +0700

    full CRUD example of temp User object

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/create-user', userController.saveUser);
-
+router.get('/login', userController.getLoginView);
+router.post('/login', userController.getUserByUsernameAndPassword);
 module.exports = router;
