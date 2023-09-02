1248460a3143afbd94675dc5f5fbebda8386cc8e
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-09-02 13:27:07 +0700

    pull request

--- controllers/helloController.js
+++ controllers/helloController.js
@@ -5,7 +5,7 @@ exports.getSeller = (req, res) => {
   res.sendFile('seller-view.html', { root: 'views' });
 };
 exports.getCustomers = (req, res) => {
-  res.sendFile('customer.html', { root: 'views' });
+  res.sendFile('customer-view.html', { root: 'views' });
 };
 exports.getProductDetail = (req, res) => {
   res.sendFile('product-details-view.html', { root: 'views' });
