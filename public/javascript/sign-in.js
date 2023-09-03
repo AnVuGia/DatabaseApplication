f46006375fee53a55bf9c5eb4936b28129e5e1f6
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-09-03 14:00:31 +0700

    b4 add bcript

--- public/javascript/sign-in.js
+++ public/javascript/sign-in.js
@@ -1,35 +1,35 @@
-const userName = document.querySelector(".user-name");
-const userNameEl = document.querySelector(".error-uname");
-const password = document.querySelector(".password");
-const passwordEl = document.querySelector(".error-password");
-const signUp = document.querySelector("#btn-sign-up")
-const submit = document.querySelector("#btn-sign-in");
+const userName = document.querySelector('.user-name');
+const userNameEl = document.querySelector('.error-uname');
+const password = document.querySelector('.password');
+const passwordEl = document.querySelector('.error-password');
+const signUp = document.querySelector('#btn-sign-up');
+const submit = document.querySelector('#btn-sign-in');
+import auth from './Module/auth.js';
+const credential = {
+  username: 'lazada_auth',
+  password: 'password',
+};
+sessionStorage.setItem('sqlUser', JSON.stringify(credential));
 
-const credential ={
-  username: "lazada_auth",
-  password: "password"
-}
-sessionStorage.setItem("sqlUser", JSON.stringify(credential));
+signUp.addEventListener('click', () => {
+  window.location.href = 'signup';
+});
 
-signUp.addEventListener("click", ()=>{
-    window.location.href = "signup"
-})
-
-userName.addEventListener("change", () => {
+userName.addEventListener('change', () => {
   if (userName.validity.valid) {
-    userName.textContent = "";
-    userNameEl.textContent ="";
-    userName.classList.remove("invalid");
+    userName.textContent = '';
+    userNameEl.textContent = '';
+    userName.classList.remove('invalid');
   } else {
     userNameError();
   }
 });
 
-password.addEventListener("change", () => {
+password.addEventListener('change', () => {
   if (password.validity.valid) {
-    password.textContent = "";
-    passwordEl.textContent ="";
-    password.classList.remove("invalid");
+    password.textContent = '';
+    passwordEl.textContent = '';
+    password.classList.remove('invalid');
   } else {
     passwordError();
   }
@@ -37,8 +37,7 @@ password.addEventListener("change", () => {
 
 // FORM VALIDATION WHEN USER PRESSES SUBMIT //
 
-submit.addEventListener("click", (event) => {
-
+submit.addEventListener('click', async (event) => {
   if (!userName.validity.valid) {
     event.preventDefault();
     userNameError();
@@ -50,65 +49,50 @@ submit.addEventListener("click", (event) => {
   }
 
   const data = {
-    user_credential: JSON.parse(sessionStorage.getItem("sqlUser")),
+    user_credential: JSON.parse(sessionStorage.getItem('sqlUser')),
     info: {
       username: userName.value,
-      password: password.value
+      password: password.value,
+    },
+  };
+  const responseData = await auth.login(data.info.username, data.info.password);
+  console.log(responseData);
+  if (responseData.msg === 'Your username or password is invalid.') {
+    userNameEl.textContent = responseData;
+    userName.classList.add('invalid');
+    userName.placeholder = '';
+  } else {
+    if (responseData.role === 'admin') {
+      window.location.href = './admin-inventory';
+      sessionStorage.setItem('user', JSON.stringify(responseData.account));
+    } else if (responseData.role === 'seller') {
+      window.location.href = './seller-product';
+      sessionStorage.setItem('user', JSON.stringify(responseData.account));
+    } else if (responseData.role === 'customer') {
+      window.location.href = './customers';
+      sessionStorage.setItem('user', JSON.stringify(responseData.account));
     }
   }
-  console.log(JSON.stringify(data));
-  let url = "/hello/login"
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data); 
-      if (data.msg ===  "Your username or password is invalid.") {
-        userNameEl.textContent = data;
-        userName.classList.add("invalid");
-        userName.placeholder = "";
-      } else {
-        if (data.role === "admin") {
-        window.location.href = "./admin-inventory";
-        sessionStorage.setItem("user", JSON.stringify(data.account));
-      }else if (data.role === "seller") {
-        window.location.href = "./seller-product";
-        sessionStorage.setItem("user", JSON.stringify(data.account));
-      }else if (data.role === "customer") {
-        window.location.href = "./customers";
-        sessionStorage.setItem("user", JSON.stringify(data.account));
-        }
-      }
-    }
-    ).catch((err) => {
-      res.json(err);
-    }
-    );
 });
 
 // FORM VALIDATION FORMULAS //
 
 function userNameError() {
-    if (userName.validity.valueMissing) {
-        userNameEl.textContent = "User Name cannot be empty";
-        userName.classList.add("invalid");
-        userName.placeholder = "";
-    }
+  if (userName.validity.valueMissing) {
+    userNameEl.textContent = 'User Name cannot be empty';
+    userName.classList.add('invalid');
+    userName.placeholder = '';
+  }
 }
 function passwordError() {
   if (password.validity.valueMissing || password.validity.tooShort) {
-    password.classList.add("invalid");
-    password.placeholder = "";
+    password.classList.add('invalid');
+    password.placeholder = '';
   }
 
   if (password.validity.valueMissing) {
-    passwordEl.textContent = "Password cannot be empty";
+    passwordEl.textContent = 'Password cannot be empty';
   } else if (password.validity.tooShort) {
-    passwordEl.textContent = "Password should be at least 8 characters";
+    passwordEl.textContent = 'Password should be at least 8 characters';
   }
 }
